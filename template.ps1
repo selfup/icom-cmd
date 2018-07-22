bb225eb8c7f0a3bf3297611d57a5942762e9205e
selfup <boudinot.regis@yahoo.com>
2018-07-22 12:40:12 -0500

    under 80 chars for readability

--- template.ps1
+++ template.ps1
@@ -5,13 +5,18 @@ $binChars = New-Object System.Collections.Generic.List[System.Object]
 $chars = $cmdString.Split(' ')
 
 foreach ($char in $chars) {
-  [Byte] $converted = [int]"0x$char"
-  $binChars.Add($converted)
+    [Byte] $converted = [int]"0x$char"
+    $binChars.Add($converted)
 }
 
 [Byte[]] $numbers = $binChars
 
-$port = new-Object System.IO.Ports.SerialPort $serialPort, 9600, None, 8, one
+$port = new-Object System.IO.Ports.SerialPort $serialPort,
+  9600,
+  None,
+  8,
+  one
+
 $port.open()
 $port.Write($numbers, 0, $numbers.Count)
 $port.Close()
